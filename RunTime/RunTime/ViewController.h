ec1f5fa03ba0c00a90d826237a951eb5b1b14539
iosliutingxin <17787148484@qq.com>
2018-06-11 21:10:54 +0800

    初始化

---
+++ RunTime/RunTime/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RunTime
+//
+//  Created by 李孔文 on 2018/6/11.
+//  Copyright © 2018年 Sunning. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ RunTime/RunTime/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  RunTime
+//
+//  Created by 李孔文 on 2018/6/11.
+//  Copyright © 2018年 Sunning. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
